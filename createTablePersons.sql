2db71957fcd6153958896feaf684a9264b4591c8
glebov <constantine.glebov@gmail.com>
2023-07-14 15:03:15 +0700

    add insert into

--- createTablePersons.sql
+++ createTablePersons.sql
@@ -8,3 +8,12 @@ create table PERSONS (
     city_of_living varchar(255),
     primary key (name, surname, age)
 );
+
+insert into hwsql.PERSONS (name, surname, age, city_of_living)
+values ('Ivan', 'Ivanov','33','Moscow');
+
+insert into hwsql.PERSONS (name, surname, age, city_of_living)
+values ('Petr', 'Petrov','35','Sochi');
+
+insert into hwsql.PERSONS (name, surname, age, city_of_living)
+values ('Kot', 'Kotov','38','Barnaul');
